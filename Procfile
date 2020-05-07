ee2bd71cefd0d34e3417ad176ebf62ad091063f7
Jacson Ritzmann <jacson.ritzmann@hotmail.com>
2020-05-07 10:17:38 -0300

    Ajuste Procfile 3

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
+
 web: npx sequelize db:migrate:all && npx sequelize db:seed:all
 web: yarn start
--- config/session.js
+++ config/session.js
@@ -1,12 +1,12 @@
-const session = require('express-session')
-const SequelizeStore = require('connect-session-sequelize')(session.Store)
-const { sequelize } = require('../app/models')
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const { sequelize } = require("../app/models");
 
 module.exports = {
-  secret: 'snippetfy2019',
+  secret: "snippetfy2020",
   resave: false,
   saveUninitialized: false,
   store: new SequelizeStore({
-    db: sequelize
-  })
-}
+    db: sequelize,
+  }),
+};
